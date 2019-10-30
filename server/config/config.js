338f3a9240f1e2c1c82ff64d7ea25c6f268f406c
unknown <richardzlcfc92@gmail.com>
2019-10-29 23:33:01 -0300

    Variable de Entorno MONGO.1

--- server/config/config.js
+++ server/config/config.js
@@ -33,8 +33,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-	//urlDB = 'mongodb://dissaor:dissaor00@cluster0-shard-00-00-hhwsv.mongodb.net:27017,cluster0-shard-00-01-hhwsv.mongodb.net:27017,cluster0-shard-00-02-hhwsv.mongodb.net:27017/cafe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
-	urlDB = process.env.MONGO_URI;
+	urlDB = 'mongodb://dissaor:dissaor00@cluster0-shard-00-00-hhwsv.mongodb.net:27017,cluster0-shard-00-01-hhwsv.mongodb.net:27017,cluster0-shard-00-02-hhwsv.mongodb.net:27017/cafe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+	//urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 
